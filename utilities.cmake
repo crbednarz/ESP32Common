2bdf89a8dcc31bbbd6ddd71d53a42e96da78c884
cr.bednarz@gmail.com <cr.bednarz@gmail.com>
2019-03-15 16:55:11 -0700

    Fixed setup_module being incompletely committed.

--- utilities.cmake
+++ utilities.cmake
@@ -1,1 +1,15 @@
 macro(setup_module name)
+	file(GLOB_RECURSE COMPONENT_SRCS
+		"public/*.c"
+		"public/*.h"
+		"public/*.hpp"
+		"public/*.cpp"
+		"private/*.c"
+		"private/*.h"
+		"private/*.hpp"
+		"private/*.cpp")
+
+	set(COMPONENT_ADD_INCLUDEDIRS "public" "private")
+
+	register_component()
+endmacro()
